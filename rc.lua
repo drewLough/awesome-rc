37d5704c429f39f55dd0f171095a2656cdd0b703
Drew Lough <dlo@soma.deftmotiveaud.io>
2017-06-06 03:31:55 -0700

    try again

--- rc.lua
+++ rc.lua
@@ -255,8 +255,8 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
 -- {{{ dlo user bindings
-    awful.key({ modkey, "Shift" }, "t", awful.titlebar.toggle),
---            {description="toggle titlebar visibility"})
+    awful.key({ modkey, "Shift" }, "t", awful.titlebar.toggle,
+              {description="toggle titlebar visibility"})
 -- {{{ end of user bindings   
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
